5ec453eb9094de3fd32839434609c8f0abe963e0
Shubham Godshalwar <sgodshalwar@gmail.com>
2019-12-08 16:31:05 -0500

    flask api changes

--- index.py
+++ index.py
@@ -2,39 +2,56 @@
 
 from flask import Flask, request
 from langdetect import detect
-
+from flask_cors import CORS,cross_origin
 from word_frequency_summarizer import WordFrequencySummarizer
 from openie_summarizer import OpenIESummarizer
+import requests
 
 text_str = '''
 Google News is a news aggregator app developed by Google. It presents a continuous, customizable flow of articles organized from thousands of publishers and magazines. Google News is available as an app on Android, iOS, and the Web. Google released a beta version in September 2002 and the official app in January 2006.
 '''
 
 app = Flask(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
 
 @app.route('/summarize', methods=['POST'])
 def summarize():
-    text = request.form['text']
-    language = detect(text)
-    summary = ''
-    error = ''
+    try:
+        data = request.get_json()
+        text = data['text']
+        print(text)
+        language = detect(text_str)
+        summary = ''
+        error = ''
+        text = text_str
 
-    if language == 'en':
-        wfs = WordFrequencySummarizer()
-        ops = OpenIESummarizer()
-        wfs_summary = wfs.summarize(text, 1.1)
-        print(wfs_summary)
-        summary = ops.summarize(wfs_summary if len(wfs_summary) > 0 else text)
+        if language == 'en':
+            language = "English"
+            wfs = WordFrequencySummarizer()
+            ops = OpenIESummarizer()
+            wfs_summary = wfs.summarize(text, 1.1)
+            print(wfs_summary)
+            summary = ops.summarize(wfs_summary if len(wfs_summary) > 0 else text)
+            summary = summary.strip()
+            print(summary)
+        elif language == 'es':
+            language = "Spanish"
+            response = requests.post('http://localhost:8080/summarize', data=data)
+            summary = response.text
+            print(response.text)
+        else:
+            error = 'The language is not supported'
         print(summary)
-    elif language == 'es':
-        error = 'Spanish summarization is in progress'
-    else:
-        error = 'The language is not supported'
-    return {
-        'language': language,
-        'summary': summary.strip(),
-        'error': error
-    }
+        return {
+            'language': language,
+            'summary': summary,
+            'error': error
+        }
+    except Exception as e:
+        print(e)
+        return {'Error':e}
 
 if __name__ == '__main__':
     app.run('localhost', 5000)
